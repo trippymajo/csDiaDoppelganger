7cdc43be7933f396fdf17bb0be421bf75fe046c0
trippymajo <elyadesu@yandex.ru>
2024-07-25 19:13:42 +0300

    W.I.P. Totally new solution...

---
+++ BamlReader/BamlShaman.cs
@@ -0,0 +1,95 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections;
+using System.IO;
+using System.Resources;
+
+namespace BamlReader
+{
+  public class BamlShaman
+  {
+    private string m_strOutPath;
+
+    private void SaveBaml(Stream streamBaml, string strResName)
+    {
+      //TODO: Check if m_strOutPath is not null
+      //Save the raw BAML data
+      using (FileStream fileStream = new FileStream(m_strOutPath, FileMode.Create, FileAccess.Write))
+        streamBaml.CopyTo(fileStream);
+
+      Console.WriteLine($"BAML content from {strResName} extracted and saved to {m_strOutPath}");
+    }
+
+    private void ExtractBamlFromResources(Stream streamRes)
+    {
+      using (ResourceReader resReader = new ResourceReader(streamRes))
+      {
+        foreach (DictionaryEntry ent in resReader)
+        {
+          //Check if entry is .baml resource
+          string strResName = ent.Key.ToString();
+          if (!strResName.EndsWith(".baml"))
+            continue;
+
+
+          //Yeah, there are actually 2 types of streams could be found
+          if (ent.Value is Stream)
+          {
+            Stream streamBaml = (Stream)ent.Value;
+            SaveBaml(streamBaml, strResName);
+          }
+          else if (ent.Value is byte[])
+          {
+            MemoryStream bamlStream = new MemoryStream((byte[])ent.Value);
+            SaveBaml(bamlStream, strResName);
+          }
+        }
+      }
+    }
+
+    public void ReadDll(string strDllPath, string strOutPath)
+    {
+      m_strOutPath = strOutPath;
+      // Load the assembly using Mono.Cecil
+      var assemblyDefinition = AssemblyDefinition.ReadAssembly(strDllPath);
+
+      foreach (var res in assemblyDefinition.MainModule.Resources)
+      {
+        if (res is EmbeddedResource embeddedResource)
+        {
+          // Get the resource stream
+          using (Stream resStream = embeddedResource.GetResourceStream())
+          {
+            ExtractBamlFromResources(resStream);
+          }
+        }
+      }
+    }
+
+    //static string ConvertBamlToXaml(string bamlFilePath)
+    //{
+    //  using (FileStream bamlStream = new FileStream(bamlFilePath, FileMode.Open, FileAccess.Read))
+    //  {
+    //    // Read BAML
+    //    var bamlReader = new Baml2006Reader(bamlStream);
+
+    //    // Create XAML writer settings
+    //    var writerSettings = new XamlObjectWriterSettings
+    //    {
+    //      RootObjectInstance = null
+    //    };
+    //    var xamlWriter = new XamlObjectWriter(bamlReader.SchemaContext, writerSettings);
+
+    //    // Read BAML and write XAML
+    //    while (bamlReader.Read())
+    //    {
+    //      xamlWriter.WriteNode(bamlReader);
+    //    }
+
+    //    // Convert the XAML object to a string
+    //    var xamlObject = xamlWriter.Result;
+    //    return XamlWriter.Save(xamlObject);
+    //  }
+    //}
+  }
+}
---
+++ DiaDoppelganger/DiaDoppelganger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+using BamlReader;
+
+internal class DiaDoppelganger
+{
+  [STAThread]
+  static void Main()
+  {
+    // Path to the DLL containing the WPF window XAML
+    string dllPath = @"C:\Users\Admin\Desktop\pseudo_seam\bin\Debug\net8.0-windows\PseudoSteam.dll";
+    string xamlPath = @"C:\Users\Admin\Desktop\MainWindow.xaml";
+    string outputPath = @"C:\Users\Admin\Desktop\Baml.txt";
+
+    BamlReader.BamlShaman bamlS = new BamlReader.BamlShaman();
+    bamlS.ReadDll(dllPath, outputPath);
+    // Start the WPF application
+    Application app = new Application();
+    app.Run();
+  }
+}
